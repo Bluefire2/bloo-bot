5957ba5f539648e2be1f0268374163ff99b57b38
Bluefire2 <chernyshov.k@gmail.com>
2017-09-26 15:42:09 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,116 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json');
+const cmdData = require('./commands.json');
+const cmd = require('./commands.js');
+
+const prefix = '~';
+
+client.on('ready', () => {
+  console.log('Hello world!');
+});
+
+client.on('message', (msg) => {
+  if(msg.author === client.user || !msg.content.startsWith(prefix)) {
+    // make sure the bot doesn't respond to its own messages
+    return;
+  } else {
+    console.log(msg.content);
+    var parsedCmd = cmdParse(msg.content), // parse out the command and args
+      output = cmdExe(msg, parsedCmd.cmdName, parsedCmd.cmdArgs);
+
+    console.log(parsedCmd);
+    var outText = '';
+
+    for(var i in output) {
+      outText += output[i];
+      if(i < output.length - 1) outText += '\n';
+    }
+
+    if(outText !== '') {
+      msg.channel.send('```\n' + outText + '```');
+    }
+  }
+});
+
+client.login(config.token);
+
+function cmdExe(msg, cmdName, args) {
+  var currCmd = cmdData[cmdName],
+    outText = [],
+    paramsCount = Object.keys(currCmd.params).length;
+
+  if(typeof currCmd === 'undefined') {
+    msg.channel.send('**Undefined command name** "' + cmdName + '"');
+    return;
+  }
+  if(args.length === 0 && paramsCount !== 0) {
+    // output the command docstring
+    var cmdParams = currCmd.params,
+      usageStr = cmdName + " <" + Object.keys(cmdParams).join("> <") + ">";
+
+    outText.push(usageStr);
+    outText.push(currCmd.desc + '\n');
+
+    for (var paramName in cmdParams) {
+      var paramDesc = cmdParams[paramName];
+      outText.push(paramName + ": " + paramDesc);
+    }
+  } else {
+    func = cmd.cmd[currCmd.fn];
+
+    var fullArgs;
+
+    fullArgs = args.slice(0);
+    fullArgs.unshift(msg);
+
+    moreText = func.apply(this, fullArgs);
+    if(typeof moreText === 'string') {
+      outText.push(moreText);
+    } else if(Array.isArray(moreText)) {
+      outText = outText.concat(moreText);
+    }
+  }
+  return outText;
+}
+
+function cmdParse(cmdString) {
+  var cmdText = cmdString.slice(prefix.length), // take out the prefix
+    firstSpace = cmdText.indexOf(' '),
+    commandName,
+    commandArgs;
+
+    if(firstSpace != -1) {
+      commandName = cmdText.slice(0, firstSpace);  // get the command name
+      commandArgs = cmdText.slice(firstSpace).match(/"(?:\\"|\\\\|[^"])*"|\S+/g)
+        .map((elem) => {
+          if(elem.charAt(0) === '"' && elem.charAt(elem.length - 1) === '"') {
+            return elem.slice(1, elem.length - 1);
+          } else {
+            return elem;
+          }
+        })
+        .map((elem) => {
+          return isNaN(elem) ? elem : +elem; // convert to number if numerical
+        }); // get the args
+
+        // if too many args have been received:
+        var paramsCount = Object.keys(cmdData[commandName].params).length;
+
+        if(commandArgs.length > paramsCount) {
+          // too many args received, so condense the remainder into one argument
+          var remainingArgs = commandArgs.slice(paramsCount - 1, commandArgs.length);
+
+          commandArgs = commandArgs.slice(0, paramsCount - 1);
+          commandArgs.push(remainingArgs.join(' '));
+        }
+    } else {
+      commandName = cmdText;
+      commandArgs = [];
+    }
+
+  return {
+    'cmdName': commandName,
+    'cmdArgs': commandArgs
+  }
+}
---
+++ commands.js
@@ -0,0 +1,146 @@
+const cheerio = require('cheerio');
+const axios = require('axios');
+const gtranslate = require('google-translate-api');
+
+const numberWithCommas = (x) => {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
+const slayerAuraChance = (tier) => {
+  var multiplier;
+  switch(tier) {
+    case 1:
+      multiplier = 1.03;
+    case 2:
+      multiplier = 1.05;
+    case 3:
+      multiplier = 1.07;
+    case 4:
+      multiplier = 1.1;
+    case 5:
+      multiplier = 1.15;
+    default:
+      multiplier = 1.0;
+
+    return 1 / (2 - multiplier); // expected value of geometric distribution
+  }
+};
+
+const randomInRange = (min, max) => {
+  return Math.floor(Math.random() * (max + 1 - min)) + min;
+};
+
+exports.cmd = {
+  import_fn: (msg, text) => {
+    if(text === 'this') {
+      return exports.cmd.pasta(msg, 'pyzen');
+    }
+  },
+  ping: (msg) => {
+    console.log(Date.now(), msg.createdTimestamp);
+    var pingTime = (Date.now() - msg.createdTimestamp);
+    msg.channel.send('Pong! ' + pingTime + 'ms');
+  },
+  roll: (msg, sides, dice = 1) => {
+    var rolls = [];
+
+    for(let i = 0; i < dice; i++) {
+      rolls.push(randomInRange(1, sides));
+    }
+
+    return rolls.join(' ');
+  },
+  pasta: (msg, pastaName) => {
+    var pastaData = require("./data/pastas.json");
+    if(pastaName !== 'list') {
+      pastaText = pastaData[pastaName];
+
+      msg.channel.send(pastaText);
+    } else {
+      return Object.keys(pastaData).slice(0);
+    }
+  },
+  pc: (msg, item, amount = 1) => {
+    const baseUrl = 'http://runescape.wikia.com/wiki/Exchange:'
+
+    axios.get(baseUrl + item)
+      .then((response) => {
+        // console.log(response.data);
+        var $ = cheerio.load(response.data),
+          price = parseInt($('#GEPrice').text().replace(/,/g, '')),
+          totalPrice = numberWithCommas(price * amount);
+
+        msg.channel.send(item + ' x ' + amount + ': ' + totalPrice + 'gp');
+      }).catch((error) => {
+        console.log(error);
+      });
+
+    return false;
+  },
+  slayer: (msg, monster, amount = 1, aura = 0) => {
+    const baseUrl = 'http://runescape.wikia.com/wiki/';
+
+    var auraMultiplier = slayerAuraChance(aura),
+      expectedAmount = parseInt(amount * auraMultiplier);
+
+    // get the monster id
+    axios.get(baseUrl + monster)
+      .then((response) => {
+        const processXpText = (text) => {
+          return Math.floor(parseFloat(text.replace(/,/g, '')) * expectedAmount);
+        }
+
+        var $ = cheerio.load(response.data),
+          monsterName = $('.page-header__title').text(),
+          xpCombat = processXpText($('.mob-cb-xp').text()),
+          xpHp = processXpText($('.mob-hp-xp').text()),
+          xpSlayer = processXpText($('.mob-slay-xp').text());
+
+        msg.channel.send(monsterName + ' x ' + expectedAmount + ': ' + xpSlayer
+          + ' slayer xp, ' + xpCombat + ' combat xp and ' + xpHp + ' hp xp.');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+  wp: (msg, article, lang = 'en') => {
+    var baseUrl = 'https://' + lang + '.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=',
+      baseLinkUrl = 'https://' + lang + '.wikipedia.org/wiki/';
+
+    axios.get(baseUrl + article)
+      .then((response) => {
+        if(response.data.query.search.length !== 0) {
+          var firstResult = response.data.query.search[0],
+            firstResultTitle = firstResult.title.replace(/ /g, '_');
+
+          msg.channel.send('Wikipedia link: ' + baseLinkUrl + firstResultTitle);
+        } else {
+          msg.channel.send('No search results found for "' + article + '"');
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        if(error.code === 'ENOTFOUND') {
+          msg.channel.send('**Invalid language code** "' + lang + '"');
+        }
+      });
+  },
+  translate: (msg, langFrom, langInto, ...text) => {
+    var textJoined = text.join(' ');
+
+    gtranslate(textJoined, {from: langFrom, to: langInto}).then((res) => {
+      var out = '';
+
+      if(res.from.text.autoCorrected || res.from.text.didYouMean) {
+        out += '**Autocorrected** "' + textJoined + '" **to** "' + res.from.text.value + '"\n\n';
+      }
+
+      out += res.text;
+
+      msg.channel.send(out);
+    }).catch(err => {
+        console.error(err);
+        msg.channel.send('**' + err.message + '**');
+    });
+  }
+};
