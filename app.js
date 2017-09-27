d20977ad2e2f2afd6b6ca54345df7f2f84e2dccd
Bluefire2 <chernyshov.k@gmail.com>
2017-09-26 22:44:35 -0400

    Copied from docs, looks interesting

--- app.js
+++ app.js
@@ -33,6 +33,16 @@ client.on('message', (msg) => {
   }
 });
 
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
 client.login(config.token);
 
 function cmdExe(msg, cmdName, args) {
